95da5952a76a5d4c357402584acfd6833a2c9c16
Darshan Manjrekar <darshanmanjrekar1205@gmail.com>
2018-11-09 15:33:11 +0530

    remove protect miyabi

--- security/selinux/selinuxfs.c
+++ security/selinux/selinuxfs.c
@@ -268,7 +268,6 @@ static const struct file_operations sel_handle_status_ops = {
 	.llseek		= generic_file_llseek,
 };
 
-#ifndef CONFIG_SECURITY_MIYABI
 #ifdef CONFIG_SECURITY_SELINUX_DISABLE
 static ssize_t sel_write_disable(struct file *file, const char __user *buf,
 				 size_t count, loff_t *ppos)
@@ -318,9 +317,6 @@ out:
 #else
 #define sel_write_disable NULL
 #endif
-#else /* CONFIG_SECURITY_MIYABI */
-#define sel_write_disable NULL
-#endif /* !CONFIG_SECURITY_MIYABI */
 
 static const struct file_operations sel_disable_ops = {
 	.write		= sel_write_disable,
@@ -1924,7 +1920,6 @@ static int __init init_sel_fs(void)
 
 __initcall(init_sel_fs);
 
-#ifndef CONFIG_SECURITY_MIYABI
 #ifdef CONFIG_SECURITY_SELINUX_DISABLE
 void exit_sel_fs(void)
 {
@@ -1933,5 +1928,3 @@ void exit_sel_fs(void)
 	unregister_filesystem(&sel_fs_type);
 }
 #endif
-#endif /* !CONFIG_SECURITY_MIYABI */
-
